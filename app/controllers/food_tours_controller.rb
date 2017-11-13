ee5f8b1441bda8ba79a0af7de36ff45c55a0bc19
Sophie Dermaux <sophiebcdermaux@gmail.com>
2017-11-13 16:17:52 +0000

    updated the routes before making controllers

---
+++ app/controllers/food_tours_controller.rb
@@ -0,0 +1,2 @@
+class FoodToursController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :food_tours, except: [ :destroy ] do
+    resources :bookings, only: [ :create ]
+  end
+  resources :bookings, only: [ :edit, :update, :destroy ]
 end
