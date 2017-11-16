7088462ebb15886f46af5c88257a207ea63fb669
Sophie Dermaux <sophiebcdermaux@gmail.com>
2017-11-16 16:23:12 +0000

    god knows what happened

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 20171116135947) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,9 +39,7 @@ ActiveRecord::Schema.define(version: 20171116135947) do
     t.float "latitude"
     t.float "longitude"
     t.string "location"
-
     t.integer "capacity"
-
     t.index ["user_id"], name: "index_food_tours_on_user_id"
   end
 
